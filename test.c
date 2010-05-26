a83d08f138a4ea0cbd105efa25e623b2ca0c251e
Carl Masak <cmasak@gmail.com>
2010-05-26 13:23:46 +0200

    [test.c] implemented coords parsing
    There's now an 'internal interface' of functions starting with an underscore,
    and an 'external interface' being tested in the main function. The internal
    functions generally do no bounds checking, so they're more light-weight but
    less safe.

--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int tests_run = 0;
 
@@ -62,45 +63,94 @@ void destroy_druid_game(druid_game *game) {
     free(game);
 }
 
-/* Coordinates are zero-based and bounds-checked. */
-int color_at(druid_game *game, int row, int col) {
-    int size;
+/* Coordinates are bounds-checked. */
+int parse_coord(druid_game *game, char *coord, char expected_stopper) {
+    int row, col, size;
+
+    if (*coord >= 'A' && *coord <= 'Z') {
+        col = *coord - 'A';
+    }
+    else if (*coord >= 'a' && *coord <= 'z') {
+        col = *coord - 'a';
+    }
+    else {
+        return ILLEGAL;
+    }
+    ++coord;
+
+    if (*coord < '0' || *coord > '9') {
+        return ILLEGAL;
+    }
+    row = *coord - '0';
+    coord++;
+    if (*coord >= '0' && *coord <= '9') {
+        row *= 10;
+        row += *coord - '0';
+        coord++;
+    }
+    row--;
+
+    if (*coord != expected_stopper) {
+        return ILLEGAL;
+    }
 
     size = game->size;
     if (row < 0 || row >= size)
         return ILLEGAL;
-    if (col < 0 || col >= size)
+    if (col >= size)
         return ILLEGAL;
 
-    return game->colors[row * size + col];
+    return row * size + col;
 }
 
-int height_at(druid_game *game, int row, int col) {
-    int size;
+/* Coordinates are bounds-checked. */
+int color_at(druid_game *game, char *coord) {
+    int pos;
 
-    size = game->size;
-    if (row < 0 || row >= size)
+    pos = parse_coord(game, coord, '\0');
+    if (pos == ILLEGAL) {
         return ILLEGAL;
-    if (col < 0 || col >= size)
-        return ILLEGAL;
+    }
 
-    return game->heights[row * size + col];
+    return game->colors[pos];
 }
 
 /* Bounds checking not made. */
-void set_color_at(druid_game *game, int row, int col, int new_color) {
+int _color_at(druid_game *game, int row, int col) {
+    return game->colors[row * game->size + col];
+}
+
+/* Coordinates are bounds-checked. */
+int height_at(druid_game *game, char *coord) {
+    int pos;
+
+    pos = parse_coord(game, coord, '\0');
+    if (pos == ILLEGAL) {
+        return ILLEGAL;
+    }
+
+    return game->heights[pos];
+}
+
+/* Bounds checking not made. */
+int _height_at(druid_game *game, int row, int col) {
+    return game->heights[row * game->size + col];
+}
+
+/* Bounds checking not made. */
+void _set_color_at(druid_game *game, int row, int col, int new_color) {
     game->colors[row * game->size + col] = new_color;
 }
 
-void set_height_at(druid_game *game, int row, int col, int new_height) {
+void _set_height_at(druid_game *game, int row, int col, int new_height) {
     game->heights[row * game->size + col] = new_height;
 }
 
-void increase_height_at(druid_game *game, int row, int col) {
+void _increase_height_at(druid_game *game, int row, int col) {
     game->heights[row * game->size + col]++;
 }
 
-void switch_player_on_turn(druid_game *game) {
+void _switch_player_on_turn(druid_game *game) {
     game->player_on_turn
         = game->player_on_turn == VERTICAL
             ? HORIZONTAL
@@ -108,82 +158,136 @@ void switch_player_on_turn(druid_game *game) {
 }
 
 /* Bounds checking not made. Returns MOVE_MADE or INVALID_MOVE. */
-int make_sarsen_move(druid_game *game, int row, int col) {
+int _make_sarsen_move(druid_game *game, int row, int col) {
     int color;
 
-    color = color_at(game, row, col);
+    color = _color_at(game, row, col);
     if (color != 0 && color != game->player_on_turn)
         return INVALID_MOVE;
 
-    set_color_at(game, row, col, game->player_on_turn);
-    increase_height_at(game, row, col);
-    switch_player_on_turn(game);
+    _set_color_at(game, row, col, game->player_on_turn);
+    _increase_height_at(game, row, col);
+    _switch_player_on_turn(game);
 
     return MOVE_MADE;
 }
 
+/* Coordinates are bounds-checked. Returns MOVE_MADE or INVALID_MOVE. */
+int make_sarsen_move(druid_game *game, char *coord) {
+    int pos, size;
+
+    pos = parse_coord(game, coord, '\0');
+    if (pos == ILLEGAL)
+        return INVALID_MOVE;
+
+    size = game->size;
+    return _make_sarsen_move(game, pos / size, pos % size);
+}
+
 /* Bounds checking not made. The variable 'col' refers to the left end of
    the lintel. */
-int make_hlintel_move(druid_game *game, int row, int col) {
+int _make_hlintel_move(druid_game *game, int row, int col) {
     int player_support, new_height, new_color, i;
 
-    if (height_at(game, row, col) != height_at(game, row, col + 2))
+    if (_height_at(game, row, col) != _height_at(game, row, col + 2))
         return INVALID_MOVE;
-    if (height_at(game, row, col + 1) > height_at(game, row, col))
+    if (_height_at(game, row, col + 1) > _height_at(game, row, col))
         return INVALID_MOVE;
 
     player_support = 0;
-    if (color_at(game, row, col) == game->player_on_turn)
+    if (_color_at(game, row, col) == game->player_on_turn)
         player_support++;
-    if (color_at(game, row, col + 2) == game->player_on_turn)
+    if (_color_at(game, row, col + 2) == game->player_on_turn)
         player_support++;
-    if (color_at(game, row, col + 1) == game->player_on_turn
-        && height_at(game, row, col + 1) == height_at(game, row, col))
+    if (_color_at(game, row, col + 1) == game->player_on_turn
+        && _height_at(game, row, col + 1) == _height_at(game, row, col))
         player_support++;
     if (player_support != 2)
         return INVALID_MOVE;
 
-    new_height = height_at(game, row, col) + 1;
+    new_height = _height_at(game, row, col) + 1;
     new_color = game->player_on_turn;
     for (i = 0; i <= 2; ++i) {
-        set_height_at(game, row, col + i, new_height);
-        set_color_at(game, row, col + i, new_color);
+        _set_height_at(game, row, col + i, new_height);
+        _set_color_at(game, row, col + i, new_color);
     }
-    switch_player_on_turn(game);
+    _switch_player_on_turn(game);
 
     return MOVE_MADE;
 }
 
 /* Bounds checking not made. The variable 'row' refers to the bottom end of
    the lintel. */
-int make_vlintel_move(druid_game *game, int row, int col) {
+int _make_vlintel_move(druid_game *game, int row, int col) {
     int player_support, new_height, new_color, i;
 
-    if (height_at(game, row, col) != height_at(game, row + 2, col))
+    if (_height_at(game, row, col) != _height_at(game, row + 2, col))
         return INVALID_MOVE;
-    if (height_at(game, row + 1, col) > height_at(game, row, col))
+    if (_height_at(game, row + 1, col) > _height_at(game, row, col))
         return INVALID_MOVE;
 
     player_support = 0;
-    if (color_at(game, row, col) == game->player_on_turn)
+    if (_color_at(game, row, col) == game->player_on_turn)
         player_support++;
-    if (color_at(game, row + 2, col) == game->player_on_turn)
+    if (_color_at(game, row + 2, col) == game->player_on_turn)
         player_support++;
-    if (color_at(game, row + 1, col) == game->player_on_turn
-        && height_at(game, row + 1, col) == height_at(game, row, col))
+    if (_color_at(game, row + 1, col) == game->player_on_turn
+        && _height_at(game, row + 1, col) == _height_at(game, row, col))
         player_support++;
     if (player_support != 2)
         return INVALID_MOVE;
 
-    new_height = height_at(game, row, col) + 1;
+    new_height = _height_at(game, row, col) + 1;
     new_color = game->player_on_turn;
     for (i = 0; i <= 2; ++i) {
-        set_height_at(game, row + i, col, new_height);
-        set_color_at(game, row + i, col, new_color);
+        _set_height_at(game, row + i, col, new_height);
+        _set_color_at(game, row + i, col, new_color);
     }
-    switch_player_on_turn(game);
+    _switch_player_on_turn(game);
 
     return MOVE_MADE;
+}
+
+/* Coordinates are bounds-checked. Returns MOVE_MADE or INVALID_MOVE. */
+int make_lintel_move(druid_game *game, char *coord) {
+    int pos1, pos2, row1, row2, temp, col1, col2, size;
+
+    pos1 = parse_coord(game, coord, '-');
+    if (pos1 == ILLEGAL)
+        return INVALID_MOVE;
+    coord = strchr(coord, '-') + 1;
+    pos2 = parse_coord(game, coord, '\0');
+    if (pos2 == ILLEGAL)
+        return INVALID_MOVE;
+
+    size = game->size;
+    row1 = pos1 / size;
+    row2 = pos2 / size;
+    if (row1 > row2) {
+        temp = row1;
+        row1 = row2;
+        row2 = temp;
+    }
+
+    col1 = pos1 % size;
+    col2 = pos2 % size;
+    if (col1 > col2) {
+        temp = col1;
+        col1 = col2;
+        col2 = temp;
+    }
+
+    if (row1 < 0 || row2 >= size)
+        return INVALID_MOVE;
+    if (col2 >= size)
+        return INVALID_MOVE;
+
+    if (row1 == row2 && col2 == col1 + 2)
+        return _make_hlintel_move(game, row1, col1);
+    else if (col1 == col2 && row2 == row1 + 2)
+        return _make_vlintel_move(game, row1, col1);
+    else
+        return INVALID_MOVE;
 }
 
 int main() {
@@ -193,24 +297,24 @@ int main() {
 
     is(game->size, 4, "game initialized with the right size");
     is(game->player_on_turn, VERTICAL, "vertical starts");
-    is(color_at(game, 1, 1), EMPTY, "the board is empty at the start");
-    is(height_at(game, 1, 1), 0, "the board is flat at the start");
+    is(color_at(game, "b2"), EMPTY, "the board is empty at the start");
+    is(height_at(game, "b2"), 0, "the board is flat at the start");
 
-    is(make_sarsen_move(game, 1, 1), MOVE_MADE, "sarsen move works");
-    is(color_at(game, 1, 1), VERTICAL, "color changed at the position");
-    is(height_at(game, 1, 1), 1, "height has increased by one");
+    is(make_sarsen_move(game, "b2"), MOVE_MADE, "sarsen move works");
+    is(color_at(game, "b2"), VERTICAL, "color changed at the position");
+    is(height_at(game, "b2"), 1, "height has increased by one");
     is(game->player_on_turn, HORIZONTAL, "it is now horizontal's turn");
 
-    is(make_sarsen_move(game, 1, 1), INVALID_MOVE, "can't put sarsen there");
-    is(make_hlintel_move(game, 1, 0), INVALID_MOVE,
+    is(make_sarsen_move(game, "b2"), INVALID_MOVE, "can't put sarsen there");
+    is(make_lintel_move(game, "a2-c2"), INVALID_MOVE,
         "lintel can't rest on a single sarsen in the middle");
-    is(make_hlintel_move(game, 1, 1), INVALID_MOVE,
+    is(make_lintel_move(game, "b2-d2"), INVALID_MOVE,
         "lintel can't rest on a single sarsen at one end");
-    is(make_hlintel_move(game, 0, 0), INVALID_MOVE,
+    is(make_lintel_move(game, "a1-c1"), INVALID_MOVE,
         "can't put lintel on ground");
 
-    make_sarsen_move(game, 0, 1);
-    make_sarsen_move(game, 3, 1);
+    make_sarsen_move(game, "b1");
+    make_sarsen_move(game, "b4");
 
     /* The board now looks like this:
 
@@ -221,21 +325,21 @@ int main() {
        1 . H . . 1
          A B C D   */
 
-    is(make_vlintel_move(game, 0, 1), INVALID_MOVE,
+    is(make_lintel_move(game, "b1-b3"), INVALID_MOVE,
         "lintel can't stick out in the air");
-    is(make_vlintel_move(game, 1, 1), INVALID_MOVE,
+    is(make_lintel_move(game, "b2-b4"), INVALID_MOVE,
         "lintel must rest on two same-colored pieces");
 
-    make_sarsen_move(game, 0, 3);
-    is(make_hlintel_move(game, 0, 1), INVALID_MOVE,
+    make_sarsen_move(game, "d1");
+    is(make_lintel_move(game, "b1-d1"), INVALID_MOVE,
         "lintel must rest on two same-colored pieces");
 
-    make_sarsen_move(game, 2, 2);
-    is(make_hlintel_move(game, 0, 1), MOVE_MADE, "lintel on two pieces");
-    is(height_at(game, 0, 1), 2, "lintel gets us to height 2 (a)");
-    is(height_at(game, 0, 2), 2, "lintel gets us to height 2 (b)");
-    is(height_at(game, 0, 3), 2, "lintel gets us to height 2 (c)");
-    is(color_at(game, 0, 2), HORIZONTAL, "lintel is placed");
+    make_sarsen_move(game, "c3");
+    is(make_lintel_move(game, "b1-d1"), MOVE_MADE, "lintel on two pieces");
+    is(height_at(game, "b1"), 2, "lintel gets us to height 2 (a)");
+    is(height_at(game, "c1"), 2, "lintel gets us to height 2 (b)");
+    is(height_at(game, "d1"), 2, "lintel gets us to height 2 (c)");
+    is(color_at(game, "c1"), HORIZONTAL, "lintel is placed");
 
     /* The board now looks like this:
 
@@ -246,16 +350,16 @@ int main() {
        1 . H H H 1
          A B C D   */
 
-    is(make_vlintel_move(game, 1, 2), INVALID_MOVE,
+    is(make_lintel_move(game, "c2-c4"), INVALID_MOVE,
         "lintel can't lie on single sarsen in the middle");
-    is(make_vlintel_move(game, 1, 1), MOVE_MADE, "lintel on two pieces");
+    is(make_lintel_move(game, "b2-b4"), MOVE_MADE, "lintel on two pieces");
 
-    make_sarsen_move(game, 0, 2);
-    is(make_vlintel_move(game, 0, 1), MOVE_MADE,
+    make_sarsen_move(game, "c1");
+    is(make_lintel_move(game, "b1-b3"), MOVE_MADE,
         "lintel on one opponent piece");
 
-    make_sarsen_move(game, 0, 3);
-    make_sarsen_move(game, 1, 1);
+    make_sarsen_move(game, "d1");
+    make_sarsen_move(game, "b2");
 
     /* The board now looks like this:
 
@@ -266,33 +370,33 @@ int main() {
        1 . V H H 1
          A B C D   */
 
-    is(make_hlintel_move(game, 0, 1), MOVE_MADE,
+    is(make_lintel_move(game, "b1-d1"), MOVE_MADE,
         "lintel on one opponent piece");
 
     destroy_druid_game(game);
     game = new_druid_game(4);
 
-    make_sarsen_move(game, 0, 0);   make_sarsen_move(game, 1, 3);
-    make_sarsen_move(game, 0, 0);   make_sarsen_move(game, 1, 3);
-    make_sarsen_move(game, 0, 1);   make_sarsen_move(game, 2, 3);
-    make_sarsen_move(game, 0, 2);   make_sarsen_move(game, 3, 3);
-    make_sarsen_move(game, 0, 2);   make_sarsen_move(game, 3, 3);
+    make_sarsen_move(game, "a1");   make_sarsen_move(game, "d2");
+    make_sarsen_move(game, "a1");   make_sarsen_move(game, "d2");
+    make_sarsen_move(game, "b1");   make_sarsen_move(game, "d3");
+    make_sarsen_move(game, "c1");   make_sarsen_move(game, "d4");
+    make_sarsen_move(game, "c1");   make_sarsen_move(game, "d4");
 
-    is(make_hlintel_move(game, 0, 0), MOVE_MADE,
+    is(make_lintel_move(game, "a1-c1"), MOVE_MADE,
         "three same-color pieces, but only two supporting (horizontal)");
 
-    is(make_vlintel_move(game, 1, 3), MOVE_MADE,
+    is(make_lintel_move(game, "d2-d4"), MOVE_MADE,
         "three same-color pieces, but only two supporting (vertical)");
-    is(make_hlintel_move(game, 0, 0), INVALID_MOVE,
+    is(make_lintel_move(game, "a1-c1"), INVALID_MOVE,
         "three same-color pieces (horizontal)");
-    make_sarsen_move(game, 1, 0);
-    is(make_vlintel_move(game, 1, 3), INVALID_MOVE,
+    make_sarsen_move(game, "a2");
+    is(make_lintel_move(game, "d2-d4"), INVALID_MOVE,
         "three same-color pieces (vertical)");
 
-    make_sarsen_move(game, 2, 0);
-    make_sarsen_move(game, 3, 0);
-    make_sarsen_move(game, 2, 0);
-    is(make_vlintel_move(game, 1, 0), INVALID_MOVE,
+    make_sarsen_move(game, "a3");
+    make_sarsen_move(game, "a4");
+    make_sarsen_move(game, "a3");
+    is(make_lintel_move(game, "a2-a4"), INVALID_MOVE,
         "lintel can't rest on a single sarsen in the middle");
 
     destroy_druid_game(game);
